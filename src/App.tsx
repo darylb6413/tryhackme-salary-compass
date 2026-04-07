a5900dfbe5927d450ada94d099f0f021265e7eec
Daryl <daryljbenson@gmail.com>
2026-04-07 18:36:54 +0100

    Add Compensation route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
+import Compensation from "./pages/Compensation.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
@@ -15,9 +16,10 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/"             element={<Index />} />
+          <Route path="/compensation" element={<Compensation />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="*"             element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
